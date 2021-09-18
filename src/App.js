621ec17b2b55701fe07137d675be8abc733abb45
confidenti-al <steve@confidenti.al>
2021-09-18 16:28:44 -0600

    added requestAccount and setGreeting with signer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
 function App() {
   const [greeting, setGreetingValue] = useState();
 
-  async function requestAccount() {}
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
 
   async function fetchGreeting() {
     if (typeof window.ehtereum !== 'undefined') {
@@ -27,7 +29,18 @@ function App() {
     }
   }
 
-  async function setGreeting() {}
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
 
   return (
     <div className="App">
